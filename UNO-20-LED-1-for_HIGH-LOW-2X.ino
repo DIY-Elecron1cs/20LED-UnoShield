2e55ec8c613a0d79bcbbd583eb47d226fe0947b7
DIY электроника <private.property240702@gmail.com>
2024-10-17 15:24:40 +0300

    Update UNO-20-LED-1-for_HIGH-LOW-2X.ino

--- UNO-20-LED-1-for_HIGH-LOW-2X.ino
+++ UNO-20-LED-1-for_HIGH-LOW-2X.ino
@@ -1,34 +1,17 @@
-int b = 19;
-int DELAY = 50;
+#define DELAY 30
+
 void setup(){
-pinMode(0, OUTPUT);
-pinMode(1, OUTPUT);
-pinMode(2, OUTPUT);
-pinMode(3, OUTPUT);
-pinMode(4, OUTPUT);
-pinMode(5, OUTPUT);
-pinMode(6, OUTPUT);
-pinMode(7, OUTPUT);
-pinMode(8, OUTPUT);
-pinMode(9, OUTPUT);
-pinMode(10, OUTPUT);
-pinMode(11, OUTPUT);
-pinMode(12, OUTPUT);
-pinMode(13, OUTPUT);
-pinMode(14, OUTPUT);
-pinMode(15, OUTPUT);
-pinMode(16, OUTPUT);
-pinMode(17, OUTPUT);
-pinMode(18, OUTPUT);
-pinMode(19, OUTPUT);
+  for(int i = 0; i <= 19; i++){
+    pinMode(i, OUTPUT);
+  }
 }
 void loop(){
-for(int i = 0; i <= b; i++){
-digitalWrite(i, LOW);
-digitalWrite(19 - i, LOW);
-delay(DELAY);
-digitalWrite(i, HIGH);
-digitalWrite(19 - i, HIGH);
-delay(DELAY);
-}
+  for(int i = 0; i <= b; i++){
+    digitalWrite(i, LOW);
+    digitalWrite(19 - i, LOW);
+    delay(DELAY);
+    digitalWrite(i, HIGH);
+    digitalWrite(19 - i, HIGH);
+   delay(DELAY);
+  }
 }
